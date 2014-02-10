04c3bd98fcb740db647fc300501f0b8b7123dcd6
Guillem Barba <guillem@nan-tic.com>
2014-02-10 12:24:24 +0100

    Removed unused file

--- __init__.py
+++ __init__.py
@@ -1,14 +1,12 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .sale import *
 from .device import *
 from .reporting import *
 
 
 def register():
     Pool.register(
-        Sale,
         PosDevice,
         module='sale_pos_esc', type_='model')
     Pool.register(
